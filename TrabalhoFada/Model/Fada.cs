f3c11236061b7909a03c22d33decaa1192c5a10e
Marcos Luiz Mittag Filho <marquinhoszx_xd@outlook.com>
2018-06-21 15:57:28 -0300

    .

---
+++ TrabalhoFada/Model/Fada.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Fada
+    {
+        public string nome;
+        public string familia;
+        public string cor;
+        public string corAsa;
+        public double tamanhoAsa;
+        public bool asaQuebrada;
+        public bool mulher;
+        public bool fazBarulho;
+        public string elemento;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Nome deve ser preenchido");
+                }
+                if (value.Count() == 1)
+                {
+                    throw new Exception("Nome deve conter no mínimo 2 caracteres");
+                }
+                nome = value;
+            }
+
+
+        }
+        public string Familia
+        {
+            get
+            {
+                return familia;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Nome deve ser preenchido");
+                }
+                if (value.Count() == 1)
+                {
+                    throw new Exception("Nome deve conter no mínimo 2 caracteres");
+                }
+                familia = value;
+            }
+        }
+        public string Cor
+        {
+            get
+            {
+                return cor;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Cor deve ser preenchido");
+                }
+                if (value.Count() == 1)
+                {
+                    throw new Exception("Cor deve conter no mínimo 2 caracteres");
+                }
+                cor = value;
+            }
+        }
+        public string CorAsa
+        {
+            get
+            {
+                return corAsa;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Cor da asa deve ser preenchido");
+                }
+                if (value.Count() == 1)
+                {
+                    throw new Exception("Cor da asa deve conter no mínimo 2 caracteres");
+                }
+                corAsa = value;
+            }
+
+        }
+        public double TamanhoAsa
+        {
+            get
+            {
+                return tamanhoAsa;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("tamanho não pode ser menor que 0");
+                }
+                if (value == null)
+                {
+                    throw new Exception("Tamanho da asa deve ser preenchido");
+                }
+                tamanhoAsa = value;
+            }
+        }
+        public bool AsaQuebrada
+        {
+            get
+            {
+                return asaQuebrada;
+            }
+
+        }
+
+    }
+}
---
+++ TrabalhoFada/TrabalhoFada/CadastroFada.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TrabalhoFada
+{
+    partial class CadastroFada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFamilia = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCorDaFada = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(290, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // txtFamilia
+            // 
+            this.txtFamilia.Location = new System.Drawing.Point(102, 85);
+            this.txtFamilia.Name = "txtFamilia";
+            this.txtFamilia.Size = new System.Drawing.Size(290, 20);
+            this.txtFamilia.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(102, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(290, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.AutoSize = true;
+            this.txtNome.Location = new System.Drawing.Point(61, 47);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(35, 13);
+            this.txtNome.TabIndex = 3;
+            this.txtNome.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Família";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cor da asa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cor da fada";
+            // 
+            // txtCorDaFada
+            // 
+            this.txtCorDaFada.Location = new System.Drawing.Point(102, 154);
+            this.txtCorDaFada.Name = "txtCorDaFada";
+            this.txtCorDaFada.Size = new System.Drawing.Size(290, 20);
+            this.txtCorDaFada.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(102, 206);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(253, 206);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // CadastroFada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 614);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtCorDaFada);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtFamilia);
+            this.Controls.Add(this.textBox1);
+            this.Name = "CadastroFada";
+            this.Text = "CadastroFada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFamilia;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCorDaFada;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
